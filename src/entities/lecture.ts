a114ca0b4644be4539642d7b2c0d44ddc58c98cf
Talita <ludmilalima@outlook.com>
2022-01-20 21:57:19 -0300

    chore: creating entities files

---
+++ src/entities/lecture.ts
@@ -0,0 +1,8 @@
+// modelo para a classe
+export class Lecture {
+    // descrição da aula
+    readonly description: string
+    constructor (description: string) {
+      this.description = description
+    }
+}
