2af2710e0d66625bf680853cf2aeafa893aa39da
Talita <ludmilalima@outlook.com>
2022-01-22 19:59:32 -0300

    fix: improving module class

--- src/entities/module.ts
+++ src/entities/module.ts
@@ -2,7 +2,7 @@ import { Lecture } from './lecture'
 // classe para um módulo de curso
 export class Module {
   // um modulo pode conter varias aulas
-  readonly lectures: Array<Lecture> = []
+  private readonly lectures: Array<Lecture> = []
   // um nome
   private name: string
   // o nome do modulo é obrigatorio na construção
@@ -13,5 +13,10 @@ export class Module {
   // metodo para adição de aulas em modulos
   add (lecture: Lecture): void {
     this.lectures.push(lecture)
+  }
+
+  // verificar se contem uma aula
+  includes (lecture: Lecture): boolean {
+    return this.lectures.includes(lecture)
   }
 }
