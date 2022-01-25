ca14890ad466494bd3f25b82e5e43e3e26404bce
Talita <ludmilalima@outlook.com>
2022-01-25 19:22:17 -0300

    test: avoiding lectures with same name

--- test/entities/course.spec.ts
+++ test/entities/course.spec.ts
@@ -34,6 +34,26 @@ describe('Course', () => {
     expect(module.numberOfLectures).toBe(1)
   })
 
+  // não repetir nome das aulas nos modulos
+  it('should not be able to have two lectures with the same name in a module', () => {
+    // dado tal modulo
+    const module = new Module('Fundamentals')
+    // add tal aula
+    const lecture: Lecture = new Lecture('Branching', 'http://youtube.com/1234')
+
+    // duplicando a aula para o teste
+    const otherLecture: Lecture = new Lecture('Branching', 'http://youtube.com/5678')
+
+    // efetuar tal vinculo
+    module.add(lecture)
+    // adicionar novamente a mesma aula
+    module.add(otherLecture)
+    // verificação do vinculo do módulo com uma aula
+    expect(module.includes(lecture)).toBeTruthy()
+    // verifica duplicidade na inserção
+    expect(module.includes(otherLecture)).toBeFalsy()
+  })
+
   // cursos podem ser subdivididos em módulos
   it('should be able to add modules to courses', () => {
     // dado tal curso
