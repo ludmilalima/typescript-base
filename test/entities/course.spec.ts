f1fcb53bb3ecd261c4100d64f9aaec322990bde8
Talita <ludmilalima@outlook.com>
2022-01-30 19:42:44 -0300

    test: able to move a lecture to a different module

--- test/entities/course.spec.ts
+++ test/entities/course.spec.ts
@@ -109,4 +109,31 @@ describe('Course', () => {
     expect(course.position(courseOverviewModule)).toBe(2)
     expect(course.position(gitModule)).toBe(3)
   })
+
+  it('should be able to move a lecture to a different module', () => {
+    const course = new Course('azure-devops', 'Continuous Delivery and DevOps with Azure DevOps: Source Control with Git')
+    const fundamentalsModule = new Module('Fundamentals')
+    const branchingLecture: Lecture = new Lecture('Branching', 'https://youtube.com/1234')
+    fundamentalsModule.add(branchingLecture)
+
+    const courseOverviewModule = new Module('Course Overview')
+    const courseOverviewLecture = new Lecture('Course Overview', 'https://youtube.com/3456')
+    courseOverviewModule.add(courseOverviewLecture)
+
+    const gitModule = new Module('Source Control with Git on Azure DevOps')
+    const introductionLecture = new Lecture('Introduction', 'https://youtube.com/6789')
+    const lecture2 = new Lecture('Lecture 2', 'https://youtube.com/6789')
+    gitModule.add(introductionLecture)
+    gitModule.add(lecture2)
+
+    course.add(fundamentalsModule)
+    course.add(courseOverviewModule)
+    course.add(gitModule)
+
+    course.moveLecture(branchingLecture, fundamentalsModule, gitModule, 2)
+
+    expect(fundamentalsModule.numberOfLectures).toEqual(0)
+    expect(gitModule.numberOfLectures).toEqual(3)
+    expect(gitModule.position(branchingLecture)).toEqual(2)
+  })
 })
