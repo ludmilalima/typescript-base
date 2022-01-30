1223626fef7e1ce922ea7531a0e4c3594aad307f
Talita <ludmilalima@outlook.com>
2022-01-30 20:06:26 -0300

    chore: update Azure message

--- src/main/server.ts
+++ src/main/server.ts
@@ -2,7 +2,7 @@ const express = require('express')
 const port = process.env.PORT || 8000
 const app = express()
 
-app.get('/', (req, res) => { res.send('Hello World from theWiseDev Platform!') })
+app.get('/', (req, res) => { res.send('Hello World from theWiseDev in Azure!') })
 
 app.listen(port, () => { console.log('Our App Is Up And Running!') })
 module.exports = app
