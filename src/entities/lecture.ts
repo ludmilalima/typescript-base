ef4e740672292cd44fb98023b7c467c73a104ddd
Talita <ludmilalima@outlook.com>
2022-01-30 21:32:49 -0300

    feat: able to add further material to lectures

--- src/entities/lecture.ts
+++ src/entities/lecture.ts
@@ -1,17 +1,28 @@
+import { Material } from './material'
+
 // classe para aulas
 export class Lecture {
-    // descrição da aula
-    readonly description: string
-    // url do video da aula
-    readonly videoUrl: string
-    // descricao é obrigatoria
+  // descrição da aula
+  readonly description: string
+  // url do video da aula
+  readonly videoUrl: string
+  // lista de materiais extras
+  private readonly materials: Array<Material> = []
 
-    constructor (description: string, videoUrl: string) {
-      this.description = description
-      this.videoUrl = videoUrl
-    }
+  constructor (description: string, videoUrl: string) {
+    this.description = description
+    this.videoUrl = videoUrl
+  }
 
-    equals (other: Lecture): boolean {
-      return this.description === other.description && this.videoUrl === other.videoUrl
-    }
+  equals (other: Lecture): boolean {
+    return this.description === other.description && this.videoUrl === other.videoUrl
+  }
+
+  add (material: Material): void {
+    this.materials.push(material)
+  }
+
+  includes (material: Material): boolean {
+    return this.materials.includes(material)
+  }
 }
