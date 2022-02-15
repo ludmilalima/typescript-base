d3904592508285757ab38b450382c17ca76cabe0
Talita <ludmilalima@outlook.com>
2022-02-14 21:57:30 -0300

    refactor: remove redundant tet

--- test/entities/module.spec.ts
+++ test/entities/module.spec.ts
@@ -8,16 +8,6 @@ describe('Module', () => {
     expect(module.includes(lecture)).toBeTruthy()
   })
 
-  it('should not be able to add the same lecture twice in a module', () => {
-    const module = new Module('Fundamentals')
-    const lecture: Lecture = new Lecture('Branching', 'https://youtube.com/1234')
-    const sameLecture: Lecture = new Lecture('Branching', 'https://youtube.com/1234')
-    module.add(lecture)
-    module.add(sameLecture)
-    expect(module.includes(lecture)).toBeTruthy()
-    expect(module.numberOfLectures).toBe(1)
-  })
-
   it('should not be able to have two lectures with same name in a module', () => {
     const module = new Module('Fundamentals')
     const lecture: Lecture = new Lecture('Branching', 'https://youtube.com/1234')
