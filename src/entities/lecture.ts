629b88e55a8364b195c27f3619d45374b59b454f
Talita <ludmilalima@outlook.com>
2022-02-15 22:57:15 -0300

    feat: handle unexisting material

--- src/entities/lecture.ts
+++ src/entities/lecture.ts
@@ -3,6 +3,7 @@ import { Material } from './material'
 import { Element } from './part'
 import { ExistingElementError } from './errors/existing-element-error'
 import { Either } from '../shared/either'
+import { UnexistingElementError } from './errors/unexisting-element-error'
 
 export class Lecture implements Element {
   private readonly materials: Container<Material> = new Container<Material>()
@@ -22,8 +23,8 @@ export class Lecture implements Element {
     return this.materials.includes(material)
   }
 
-  remove (material: Material): void {
-    this.materials.remove(material)
+  remove (material: Material): Either <UnexistingElementError, void> {
+    return this.materials.remove(material)
   }
 
   equals (other: Lecture): boolean {
